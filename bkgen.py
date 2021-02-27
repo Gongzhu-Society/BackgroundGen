1bc4c1e9f7f570474070c976baff4c303b6a6451
DELL-XPS <syouran0508@gmail.com>
2021-02-27 14:46:30 +0800

    switch to PIL which is much more convinent than plt

--- bkgen.py
+++ bkgen.py
@@ -4,26 +4,21 @@ import numpy,random
 from scipy.spatial import Delaunay
 #import matplotlib
 #matplotlib.use('Agg')
-import matplotlib.pyplot as plt
+#import matplotlib.pyplot as plt
+from PIL import Image,ImageDraw
 
-def gen_tri(HEIGHT=9,WIDTH=16,N_inside=30,N_edge_width=5,N_edge_height=4,margin=0.05):
-    if N_edge_width==None:
-        N_edge_width=int(N_edge_height/HEIGHT*WIDTH)
-    if N_edge_height==None:
-        N_edge_height=int(N_edge_width/WIDTH*HEIGHT)
-
-    w_inf=WIDTH*margin
-    w_sup=WIDTH*(1-margin)
-    h_inf=HEIGHT*margin
-    h_sup=HEIGHT*(1-margin)
-    pts=[[random.uniform(w_inf,w_sup),random.uniform(h_inf,h_sup)] for i in range(N_inside)]+\
-        [[0,random.uniform(h_inf,h_sup)] for i in range(N_edge_height)]+\
-        [[WIDTH,random.uniform(h_inf,h_sup)] for i in range(N_edge_height)]+\
-        [[random.uniform(w_inf,w_sup),0] for i in range(N_edge_width)]+\
-        [[random.uniform(w_inf,w_sup),HEIGHT] for i in range(N_edge_width)]+\
-        [[0,0],[0,HEIGHT],[WIDTH,0],[WIDTH,HEIGHT]]
-    pts=numpy.array(pts)
-    tri=Delaunay(pts)
+def gen_tri(HEIGHT=1080,WIDTH=1920,N_inside=30,N_edge_width=5,N_edge_height=4,margin=0.05):
+    w_inf=int(WIDTH*margin)
+    w_sup=int(WIDTH*(1-margin))
+    h_inf=int(HEIGHT*margin)
+    h_sup=int(HEIGHT*(1-margin))
+    pts=[(random.randint(w_inf,w_sup),random.randint(h_inf,h_sup)) for i in range(N_inside)]
+    pts+=[(0,random.randint(h_inf,h_sup)) for i in range(N_edge_height)]
+    pts+=[(WIDTH,random.randint(h_inf,h_sup)) for i in range(N_edge_height)]
+    pts+=[(random.randint(w_inf,w_sup),0) for i in range(N_edge_width)]
+    pts+=[(random.randint(w_inf,w_sup),HEIGHT) for i in range(N_edge_width)]
+    pts+=[(0,0),(0,HEIGHT),(WIDTH,0),(WIDTH,HEIGHT)]
+    tri=Delaunay(numpy.array(pts))
     return pts,tri
 
 class ColorGen():
@@ -38,15 +33,25 @@ class ColorGen():
             return self.colors_rand(*self.extra_paras)
 
     def colors_rand(self):
-        #numpy.array([(random.random(),random.random(),random.random()) for t in self.ts])
-        #return ['#0f0f0f' for t in self.ts]
-        return numpy.array([random.random() for t in self.ts])
+        #return numpy.array([random.random() for t in self.ts])
+        return [(random.randint(0,255),random.randint(0,255),random.randint(0,255)) for t in self.ts]
+
 
 if __name__=="__main__":
-    pts,tri=gen_tri()
+    pts,tri=gen_tri(N_inside=30)
     cg=ColorGen(tri.simplices,pts)
     colors=cg.gen_colors()
-    fig=plt.figure(figsize=[16,9],dpi=120) #16*120=1920
+
+    img=Image.new('RGB',(1920,1080)) #,(255, 255, 255)) # default black
+    draw=ImageDraw.Draw(img)
+    for i,t in enumerate(tri.simplices):
+        draw.polygon([pts[t[0]],pts[t[1]],pts[t[2]]],fill=colors[i])
+    img.show()
+    img.save('bkg.png')
+    img.save('bkg.jpg')
+
+"""
+    fig=plt.figure(figsize=[16,9],dpi=30) #16*120=1920
     ax=fig.subplots(1)
     #ax.triplot(pts[:,0],pts[:,1],tri.simplices,color='green', marker='o')
     #ax.plot(pts[:,0],pts[:,1],'o')
@@ -54,6 +59,7 @@ if __name__=="__main__":
     plt.gca().set_aspect('equal')
     plt.show()
     fig.savefig("bkg.png")
+"""
 
 """
 help(tri)
